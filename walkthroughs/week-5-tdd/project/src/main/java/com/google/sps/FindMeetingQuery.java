0d588310026435cdcad270fbc67739b5543c2412
Peter Tone <peter2parque@gmail.com>
2020-07-16 15:19:30 +0000

    Implement query() for mandatory attendees

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,46 @@
 
 package com.google.sps;
 
-import java.util.Collection;
+import java.util.*;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+        List<TimeRange> relevantTimes = removeIrrelevant(events, request.getAttendees());
+
+        Collections.sort(relevantTimes, TimeRange.ORDER_BY_START);
+
+        Collection<TimeRange> openTimes = new ArrayList<>();
+        int intervalStart = TimeRange.START_OF_DAY;
+
+        for (TimeRange range : relevantTimes) {
+            if (range.start() > intervalStart && range.start()-intervalStart >= request.getDuration()) {
+                openTimes.add(TimeRange.fromStartEnd(intervalStart, range.start(), false));
+            } 
+            if (range.end() > intervalStart) {
+                intervalStart = range.end();
+            }
+        }
+        if (TimeRange.END_OF_DAY > intervalStart && TimeRange.END_OF_DAY-intervalStart >= request.getDuration()) {
+            openTimes.add(TimeRange.fromStartEnd(intervalStart, TimeRange.END_OF_DAY, true));
+        }
+        return openTimes;
+    }
+
+    /**
+    * Takes a set of meetings and a list of people, and returns a list of only the timeranges 
+    * from those meetings where those people are busy.
+    */
+    private List<TimeRange> removeIrrelevant(Collection<Event> events, Collection<String> requestAttendees) {
+        List<TimeRange> relevantTimes = new ArrayList<>();
+        for (Event event : events) {
+            Collection<String> eventAttendees = event.getAttendees();
+            for (String attendee : requestAttendees) {
+                if (eventAttendees.contains(attendee)) {
+                    relevantTimes.add(event.getWhen());
+                    break;
+                }
+            }
+        }
+        return relevantTimes;
+    }
 }
